bd1f2499349ac5e7813dc0abf21e14244d31e7e8
IgnacioCruz94 <90223195+IgnacioCruz94@users.noreply.github.com>
2021-10-24 23:43:28 -0500

    Add files via upload

---
+++ src/resources/booksResources.js
@@ -0,0 +1,16 @@
+// Modules
+const express = require('express');
+const BooksResources = express.Router();
+const { validateInput } = require("../validations/validator");
+
+// Controllers
+const { BookControllers } = require('../controllers');
+
+// All user resources
+BooksResources.get('/', BookControllers.getAll);
+BooksResources.post('/', validateInput, BookControllers.createUser);
+BooksResources.get('/:guid', BookControllers.getByGuid);
+BooksResources.put('/:guid', BookControllers.updateUser);
+BooksResources.delete('/:guid', BookControllers.deleteUser);
+
+module.exports = BooksResources;
---
+++ src/resources/index.js
@@ -0,0 +1,5 @@
+const BooksResources = require('./booksResources');
+
+module.exports = {
+  BooksResources,
+};
